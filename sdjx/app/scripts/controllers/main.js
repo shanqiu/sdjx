f7ac7f5c4268ddba3c8ace03a5ed729d7d5f9ffc
wentong <went783@newschool.edu>
2014-09-09 19:38:34 +0800

    add videojs

--- sdjx/app/scripts/controllers/main.js
+++ sdjx/app/scripts/controllers/main.js
@@ -8,27 +8,11 @@
  * Controller of the sdjxApp
  */
 var app = angular.module('sdjxApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,$sce) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    $scope.code = ['XNzc1NTU3NTMy','XNzYwMDc0OTgw'] ; 
+    
   });
-app.directive('myYouku', function($sce) {
-  return {
-    restrict: 'EA',
-    scope: { code:'=' },
-    replace: true,
-    template: '<div style="height:400px;"><iframe style="overflow:hidden;height:100%;width:100%" width="100%" height="100%" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>',
-    link: function (scope) {
-        console.log('here');
-        scope.$watch('code', function (newVal) {
-           if (newVal) {
-               scope.url = $sce.trustAsResourceUrl("http://player.youku.com/embed/" + newVal);
-           }
-        });
-    }
-  };
-});
